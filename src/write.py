9306091e38e9778acc1118263a8b4402dfeefdd4
Tomás Correia <mhpatofrango@gmail.com>
2023-10-16 15:30:56 +0100

    Temporarily disable audio output

--- src/write.py
+++ src/write.py
@@ -1,17 +1,19 @@
 import curses
-import winsound
+import time
+# import winsound
 
 
-DELAY = 20
+DELAY = 0.02
 
 
-def write(screen, text) -> None:
+def write(window, text: str) -> None:
     """
     Prints given text at a steady speed in the terminal,
     while emitting a beeping sound. 
     """
 
     for char in text:
-        screen.addstr(char, curses.color_pair(1))
-        screen.refresh()
-        winsound.Beep(500, DELAY) # The Beep function purposely delays the loop
+        window.addstr(char, curses.color_pair(1))
+        window.refresh()
+        time.sleep(DELAY)
+        # winsound.Beep(500, DELAY) # The Beep function purposely delays the loop
