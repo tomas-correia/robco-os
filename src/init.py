8541d0c0921378393f3734d7f0877d2adbef34e3
Tomás Correia <mhpatofrango@gmail.com>
2023-10-16 03:22:34 +0100

    Rename stdscr to window

--- src/init.py
+++ src/init.py
@@ -7,18 +7,18 @@ from .screens import getScreenText
 from .colors import NORMAL_COLOR, SELECTED_COLOR
 
 
-def init(stdscr):
+def init(window):
     """
     Initializes values and calls the home screen function.
     """
     curses.init_pair(NORMAL_COLOR, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(SELECTED_COLOR, curses.COLOR_BLACK, curses.COLOR_GREEN)
-    boot(stdscr)
+    boot(window)
 
 
-def boot(stdscr):
+def boot(window):
     """
     Gets the first screen and renders it.
     """
-    write(stdscr, getScreenText(0)) # Display the first screen
-    stdscr.getch()
+    write(window, getScreenText(0)) # Display the first screen
+    window.getch()
